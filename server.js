4e72d02920087cfb784583169c8b507b1a46c188
Mirza Raheel <raheel1195@gmail.com>
2025-11-21 10:35:19 +0500

    api key and secret change

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.get("/dashboard", async (req, res) => {
 
   // Render simple HTML
   res.send(`
-    <h1>Shopify App Dashboard Non Embed APP</h1>
+    <h1>Shopify App Dashboard</h1>
     <h2>Products List</h2>
     <ul>${products.map((p) => `<li>${p.title} (${p.id})</li>`).join("")}</ul>
     <h2>Customers List</h2>
